be0216fff08a5bf05cb566b1069fb9d75ad70aa0
Vishesh <visheshsingh16@gmail.com>
2018-07-04 13:54:26 -0500

    fetching data from api

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
-console.log(123);
+import fetchJsonp from "fetch-jsonp";
+
+const petForm = document.querySelector("#pet-form");
+
+petForm.addEventListener("submit", fetchAnimals);
+
+//Fetch Animal From API
+function fetchAnimals(e) {
+  e.preventDefault();
+
+  //Get User Input
+  const animal = document.querySelector("#animal").value;
+  const zip = document.querySelector("#zip").value;
+
+  // Fetch Pets
+  fetchJsonp(
+    `http://api.petfinder.com/pet.find?format=json&key=64719e072566097abd5e913c810e3364&animal=${animal}&location=${zip}&callback=callback`,
+    {
+      jsonpCallbackFunction: "callback"
+    }
+  )
+    .then(res => res.json())
+    .then(data => showAnimals(data.petfinder.pets.pet))
+    .catch(err => console.log(err));
+}
+
+// SHow listings of pets
+function showAnimals(pets) {
+  const results = document.querySelector("#results");
+
+  //clear first
+  results.innerHTML = "";
+  //Loop thru pets
+  pets.forEach(pet => {
+    const div = document.createElement("div");
+    div.classList.add("card", "card-body", "mb-3");
+    div.innerHTML = `
+        <div class="row">
+            <div class="col-sm-6">
+                <h4>${pet.name.$t} (${pet.age.$t})</h4>
+            </div>
+            <div class="col-sm-6">
+
+            </div>
+        </div>
+      `;
+
+    results.appendChild(div);
+  });
+}
